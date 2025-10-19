f6aada74f60d611817570199b0e475d874d7dd4a
= <=>
2025-10-19 18:09:10 -0300

    Changes Login UseCase return type with testings

--- src/domain/use-cases/auth/login.ts
+++ src/domain/use-cases/auth/login.ts
@@ -1,12 +1,15 @@
 import { Email, UseCase } from "../../types";
+import { JWT } from "../../types/jwt";
 
 export interface LoginPayload {
   email: Email;
   password: string;
 }
 
-export class LoginUseCase implements UseCase<string, never> {
-  async execute({ email }: LoginPayload) {
-    return email;
+export class LoginUseCase implements UseCase<JWT, Error> {
+  async execute({ email, password }: LoginPayload) {
+    const result: JWT = `email.email.email`;
+    if (password === "") return new Error();
+    return result;
   }
 }
